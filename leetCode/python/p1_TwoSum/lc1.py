d12b434d327788d33677cf84e0b08a48d485e358
milostiv <milostrifkovic89@gmail.com>
2020-11-07 11:14:11 +0100

    Dictionary version

--- leetCode/python/p1_TwoSum/lc1.py
+++ leetCode/python/p1_TwoSum/lc1.py
@@ -8,32 +8,36 @@ class Solution:
 
     You can return the answer in any order. 
     """ 
-    def twoSum(self, nums, target): 
+    def __init__(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
+        """
+        self.nums = nums
+        self.target = target
+
+    def twoSum(self): 
+        """
         :rtype: List[int]
         """
         sum = {}
-        for i in range(len(nums)):
-            difference = target - nums[i]    
+        for i in range(len(self.nums)):
+            difference = self.target - self.nums[i]    
             if difference in sum:
                 return [sum.get(difference), i]
             else: 
-                sum[nums[i]] = i
+                sum[self.nums[i]] = i
 
-    def testFunction(nums, target, solution, testNum): 
+    def testFunction(self, solution, testNum): 
         """
-        :type nums: List[int]
-        :type target: int
         :type solution: List[int]
         :type testNum: int 
         """
         answer = []
         start_time = time.time()
-        answer = Solution.twoSum(answer, nums, target)
+        answer = self.twoSum() 
         end_time = time.time()
-        if(answer[0] != -1):
+        if(answer[0] == solution[0] and answer[1] == solution[1]):
             print('TEST' + str(testNum) + ': PASSED IN ' + '{:.2f}us'.format((end_time - start_time) * 1000000))
         else:
             print('TEST FAILED')        
@@ -44,7 +48,8 @@ nums = [2,7,11,15]
 target = 9
 solution = [0, 1]
 
-Solution.testFunction(nums, target, solution, 1)
+s = Solution(nums, target)
+s.testFunction(solution, 1)
 
 # Test 2
 print()
@@ -52,7 +57,8 @@ nums = [3, 2, 4]
 target = 6
 solution = [1, 2]
 
-Solution.testFunction(nums, target, solution, 2)
+s = Solution(nums, target)
+s.testFunction(solution, 2)
 
 # Test 3
 print()
@@ -60,4 +66,5 @@ nums = [3, 3]
 target = 6
 solution = [0, 1]
 
-Solution.testFunction(nums, target, solution, 3)
+s = Solution(nums, target)
+s.testFunction(solution, 3)

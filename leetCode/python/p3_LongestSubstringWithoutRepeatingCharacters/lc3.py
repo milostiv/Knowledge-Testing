b40a10f02b860cde9a06e9f4deb680b2182f747a
milostiv <milostrifkovic89@gmail.com>
2020-11-11 11:33:25 +0100

    Longest Substring Without Repeating Characters

--- leetCode/python/p3_LongestSubstringWithoutRepeatingCharacters/lc3.py
+++ leetCode/python/p3_LongestSubstringWithoutRepeatingCharacters/lc3.py
@@ -1,38 +1,54 @@
+import time
+
 class Solution:
     """
     Given a string s, find the length of the longest substring without repeating characters. 
     """
     def __init__(self, input_string: str):
-        self.string = inputString
+        self.string = input_string
     
     def lengthOfLongestSubstring(self) -> int:
-        substring_letters = {}
+        substring = {}
         curr_substring_start = 0
         curr_len = 0         
         longest = 0
         
         for i, letter in enumerate(self.string): 
-            if letter in sub and sub[letter] >= curr_substring_start:
-                curr_substring_start = sub[letter] + 1
-                curr_len = i - sub[letter] # Ovo verovatno moze da se izbaci... 
-                sub[letter] = i
+            if letter in substring and substring[letter] >= curr_substring_start:
+                curr_substring_start = substring[letter] + 1
+                curr_len = i - substring[letter] 
+                substring[letter] = i
             else:
-                sub[letter] = i
+                substring[letter] = i
                 curr_len += 1
-                if curr_len > longest
+                if curr_len > longest:
                     longest = curr_len
 
         return longest
      
     def testFunction(self, sol_num: int, test_num: int):
-        pass
-
+        start_time = time.time() 
+        sol = self.lengthOfLongestSubstring() 
+        end_time = time.time()
+        if(sol == sol_num):
+            print('\nTEST' + str(test_num) + ': PASSED IN ' + '{:.2f}us'.format((end_time - start_time) * 1000000))
+        else:
+            print('\nTEST FAILED')
+            
 # Test 1
 
+string = 'abcabcbb' 
+s = Solution(string)
+s.testFunction(3, 1)
 
 # Test 2
 
+string = 'bbbbb' 
+s = Solution(string)
+s.testFunction(1, 2)
 
 # Test 3
 
-
+string = 'pwwkew' 
+s = Solution(string)
+s.testFunction(3, 3)

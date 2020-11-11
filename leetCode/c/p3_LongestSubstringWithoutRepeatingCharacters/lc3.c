75a685a955f6278a2c336f265496260fe9e27532
milostiv <milostrifkovic89@gmail.com>
2020-11-11 11:52:14 +0100

    Not yet finished

---
+++ leetCode/c/p3_LongestSubstringWithoutRepeatingCharacters/lc3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int lengthOfLongestSubstring(char * s) {
+    
+printf("%ld", sizeof(s));
+}
+
+int main(void) {
+   
+    char str[] = "abcabcbb";
+     
+    lengthOfLongestSubstring(str);
+    
+    return EXIT_SUCCESS;
+}

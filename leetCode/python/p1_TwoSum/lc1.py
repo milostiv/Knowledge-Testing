10a4ef346b9759f14481261a5bbed595f0f4d215
milostiv <milostrifkovic89@gmail.com>
2020-11-21 18:01:10 +0100

    Two Sum

--- leetCode/python/p1_TwoSum/lc1.py
+++ leetCode/python/p1_TwoSum/lc1.py
@@ -8,37 +8,34 @@ class Solution:
 
     You can return the answer in any order. 
     """ 
-    def __init__(self, nums: list, target: int):
-        self.nums = nums
-        self.target = target
-
-    def two_sum(self): 
+    def two_sum(self, nums: list, target: int) -> list: 
         sum = {}
-        for i in range(len(self.nums)):
-            difference = self.target - self.nums[i]    
+        for i in range(len(nums)):
+            difference = target - nums[i]    
             if difference in sum:
                 return [sum.get(difference), i]
             else: 
-                sum[self.nums[i]] = i
+                sum[nums[i]] = i
 
-    def test_function(self, solution: list, test_num: int): 
+    def test_function(self, nums: list, target: int, sol: list, test_num: int): 
         answer = []
         start_time = time.time()
-        answer = self.two_sum() 
+        answer = self.two_sum(nums, target) 
         end_time = time.time()
-        if(answer[0] == solution[0] and answer[1] == solution[1]):
+        if(answer[0] == sol[0] and answer[1] == sol[1]):
             print('\nTEST' + str(test_num) + ': PASSED IN ' + '{:.2f}us'.format((end_time - start_time) * 1000000))
         else:
             print('\nTEST FAILED')        
+
+s = Solution()
 
 # Test 1
 
 nums = [2,7,11,15]
 target = 9
-solution = [0, 1]
+sol = [0, 1]
 
-s = Solution(nums, target)
-s.test_function(solution, 1)
+s.test_function(nums, target, sol, 1)
 
 # Test 2
 
@@ -46,8 +43,7 @@ nums = [3, 2, 4]
 target = 6
 solution = [1, 2]
 
-s = Solution(nums, target)
-s.test_function(solution, 2)
+s.test_function(nums, target, solution, 2)
 
 # Test 3
 
@@ -55,5 +51,4 @@ nums = [3, 3]
 target = 6
 solution = [0, 1]
 
-s = Solution(nums, target)
-s.test_function(solution, 3)
+s.test_function(nums, target, solution, 3)

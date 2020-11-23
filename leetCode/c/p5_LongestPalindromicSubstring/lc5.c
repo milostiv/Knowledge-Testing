34199b119a2886f2bbe15c60a84b3f7209cc437a
milostiv <milostrifkovic89@gmail.com>
2020-11-23 21:57:28 +0100

    Not yet finished

--- leetCode/c/p5_LongestPalindromicSubstring/lc5.c
+++ leetCode/c/p5_LongestPalindromicSubstring/lc5.c
@@ -3,40 +3,48 @@
 #include <string.h>
 #include <time.h>
 
-int findPalindromeSize(int l, int r, char *s) {
+struct Palindrome {
+    int size;
+    int leftIndex;
+    int rightIndex;
+};
+
+struct Palindrome findPalindromeStats(int l, int r, char *s) {
     
-    int palindromeLength = 0;
+    struct Palindrome pal;  
+    pal.size = 0;
+    pal.leftIndex = l;
+    pal.rightIndex = r;
      
-    while(l >= 0 && r < strlen(s) && s[l] == s[r]) {
-        l--;
-        r++;
-        palindromeLength++;
+    while(pal.leftIndex >= 0 && pal.rightIndex < strlen(s) && s[pal.leftIndex] == s[pal.rightIndex]) {
+        pal.leftIndex--;
+        pal.rightIndex++; 
+        pal.size++;
     }
 
-    return palindromeLength;
+    return pal;
 }
 
 char* findPalindrome(int l, int r, char *s) {
     
-    int palindromeLength = 0;
-     
-    while(l >= 0 && r < strlen(s) && s[l] == s[r]) {
-        l--;
-        r++;
-        palindromeLength++;
-    }
-     
-    char *tmp = (char *) malloc(palindromeLength * sizeof(char));
+    struct Palindrome pal = findPalindromeStats(l, r, s);  
+      
+    char *tmp = (char *) malloc(pal.size * sizeof(char));
 
-    for(int i=l; i<=r; i++)
+    for(int i = pal.leftIndex; i <= pal.rightIndex; i++)
         tmp[i] = s[i];
 
     return tmp;
 }
 
 char* longestPalindrome(char *s) {
-
-
+    
+    char *palindromeString = (char *) malloc(strlen(s) * sizeof(char));
+    
+    for(int i=0; i<strlen(s); i++) {
+        // odd case
+                
+    }
 }
 
 int main(void) {

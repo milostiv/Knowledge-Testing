daf2b5b3999fa40c62ceeea9657c26e297e2daf4
milostiv <milostrifkovic89@gmail.com>
2020-11-24 10:24:38 +0100

    Not yet finished

--- leetCode/c/p5_LongestPalindromicSubstring/lc5.c
+++ leetCode/c/p5_LongestPalindromicSubstring/lc5.c
@@ -3,69 +3,8 @@
 #include <string.h>
 #include <time.h>
 
-enum PalindromeType {
-    Even,
-    Odd
-};
-
-struct Palindrome {
-    int size;
-    int leftIndex;
-    int rightIndex;
-    enum PalindromeType type;
-};
-
-struct Palindrome findPalindromeStats(int l, int r, char *s) {
-    
-    struct Palindrome pal;  
-    pal.size = 0;
-    pal.leftIndex = l;
-    pal.rightIndex = r;
-    pal.type = Odd;
-     
-    while(pal.leftIndex >= 0 && pal.rightIndex < strlen(s) && s[pal.leftIndex] == s[pal.rightIndex]) {
-        pal.leftIndex--;
-        pal.rightIndex++; 
-        pal.size++;
-    }
-
-    return pal;
-}
-
-char* findPalindrome(struct Palindrome pal, char *s) {
-     
-    char *tmp = (char *) malloc(pal.size * sizeof(char));
-
-    for(int i = pal.leftIndex; i <= pal.rightIndex; i++)
-        tmp[i] = s[i];
-
-    return tmp;
-}
-
 char* longestPalindrome(char *s) {
     
-    struct Palindrome longestPalindrome;
-    struct Palindrome tmpPalindrome;
-    longestPalindrome.size = 0;
-    
-    for(int i=0; i<strlen(s); i++) {
-        // odd case
-        tmpPalindrome = findPalindromeStats(i, i, s);
-        if(tmpPalindrome.size > longestPalindrome.size) 
-            longestPalindrome = tmpPalindrome;                      
-        // even case
-        tmpPalindrome = findPalindromeStats(i, i+1, s);
-        if(tmpPalindrome.size > longestPalindrome.size) {
-            longestPalindrome = tmpPalindrome;
-            longestPalindrome.type = Even;
-        }
-    }
- 
-    char *palindromeString = (char *) malloc(longestPalindrome.size * sizeof(char));
-
-    palindromeString = findPalindrome(longestPalindrome, s);
-
-    return palindromeString;
 }
 
 int main(void) {

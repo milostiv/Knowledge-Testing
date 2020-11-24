453381c710ed29c3dbf964b38d26d41abe3985c6
milostiv <milostrifkovic89@gmail.com>
2020-11-24 12:02:43 +0100

    Not yet finished

--- leetCode/c/p5_LongestPalindromicSubstring/lc5.c
+++ leetCode/c/p5_LongestPalindromicSubstring/lc5.c
@@ -3,11 +3,24 @@
 #include <string.h>
 #include <time.h>
 
+struct Palindrome {
+    int size;
+    int leftIndex;
+    int rightIndex;
+};
+
+void printPalindromeInfo(struct Palindrome pal) {
+    
+    printf("Palindrome:\nSize: %d\nLeft index:%d\nRight index:%d\n", pal.size, pal.leftIndex, pal.rightIndex);
+}
+
 char* longestPalindrome(char *s) {
     
 }
 
 int main(void) {
-     
+    
+    char s[] = "babad"; 
+ 
     return EXIT_SUCCESS;
 }

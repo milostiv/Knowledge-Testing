ea0b935874a6a4a02a905fae3c1e83fdcd9a91d2
milostiv <milostrifkovic89@gmail.com>
2020-11-30 19:49:33 +0100

    String To Integer

--- leetCode/python/p8_StringToInteger_atoi/lc8.py
+++ leetCode/python/p8_StringToInteger_atoi/lc8.py
@@ -1,4 +1,4 @@
-import time 
+import time
 
 class Solution:
     """
@@ -15,7 +15,50 @@ class Solution:
 
     If no valid conversion could be performed, a zero value is returned.
     """
-    def myAtoi(self, s: str) -> int:
-        pass
+    def myAtoi(self, s: str) -> int:        
+        first_digit = False
+        sign = 1
+        n = 0
+        i = 0
+        
+        while i < len(s):
+            if first_digit == False:
+                # discard white characters
+                while s[i] == ' ' and i < len(s):
+                    i += 1
 
+                #check for sign
+                if s[i] == '-' and i < len(s):
+                    sign *= -1
+                    i += 1
+            
+            # get next digit
+            if s[i].isdigit():
+                n = n * 10 + ord(s[i]) - ord('0')
+                first_digit = True
+                i += 1
+            else:
+                break
+             
+        return n * sign
 
+    def test_function(self, s: str, sol: int, test_num: int): 
+        start_time = time.time() 
+        tmp = self.myAtoi(s)
+        end_time = time.time()
+        
+        if tmp == sol:
+            print('\nTEST' + str(test_num) + ': PASSED IN ' + '{:.2f}us'.format((end_time - start_time) * 1000000))            
+        else:
+            print('\nTEST FAILED') 
+
+# Test
+
+s1 = '42'
+s2 =  '   -42'
+s3 = '4193 with words'
+
+s = Solution()
+n = s.test_function(s1, 42, 1)
+n = s.test_function(s2, -42, 2)
+n = s.test_function(s3, 4193, 3)

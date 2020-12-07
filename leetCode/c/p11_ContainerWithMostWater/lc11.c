d88e6b4e61bb8d48977a16818d7d2d15af1f261a
milostiv <milostrifkovic89@gmail.com>
2020-12-07 22:24:39 +0100

    Container With Most Water

--- leetCode/c/p11_ContainerWithMostWater/lc11.c
+++ leetCode/c/p11_ContainerWithMostWater/lc11.c
@@ -1,6 +1,5 @@
 /* Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two end    
-points of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains 
-    
+points of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains     
 the most water. */
 
 #include <stdio.h>

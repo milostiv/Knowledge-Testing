9e354bfd876c35dd1d9a9c92269e36f9bca16778
milostiv <milostrifkovic89@gmail.com>
2021-01-07 17:29:35 +0100

    Two Sum

--- leetCode/python/p1_TwoSum/lc1.py
+++ leetCode/python/p1_TwoSum/lc1.py
@@ -22,7 +22,7 @@ class Solution:
         start_time = time.time()
         answer = self.two_sum(nums, target) 
         end_time = time.time()
-        if(answer[0] == sol[0] and answer[1] == sol[1]):
+        if answer[0] == sol[0] and answer[1] == sol[1]:
             print('\nTEST' + str(test_num) + ': PASSED IN ' + '{:.2f}us'.format((end_time - start_time) * 1000000))
         else:
             print('\nTEST FAILED')        

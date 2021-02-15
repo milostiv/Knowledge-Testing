1c59ba5f50bf2bd711561c19ed1e69b5d3179fa4
milostiv <milostrifkovic89@gmail.com>
2021-02-15 21:02:33 +0100

    Not yet finished

---
+++ leetCode/python/p10_RegularExpressionMatching/lc10.py
@@ -0,0 +1,12 @@
+import time
+
+class Solution:
+    """
+    Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*' where: 
+    '.' Matches any single character.
+    '*' Matches zero or more of the preceding element.
+    
+    The matching should cover the entire input string (not partial)
+    """
+    def isMatch(self, s: str, p: str) -> bool:
+        pass

c3a297a83f247917e6157f2a3524cd6d37f02eff
milostiv <milostrifkovic89@gmail.com>
2021-02-15 21:51:03 +0100

    Not yet finished

--- leetCode/python/p10_RegularExpressionMatching/lc10.py
+++ leetCode/python/p10_RegularExpressionMatching/lc10.py
@@ -9,4 +9,20 @@ class Solution:
     The matching should cover the entire input string (not partial)
     """
     def isMatch(self, s: str, p: str) -> bool:
-        pass
+        rows, columns = len(s), len(p)
+         
+        # Both strings empty
+        if rows == 0 and columns == 0: 
+            return True
+
+        # Pattern empty
+        if columns == 0:                           
+            return False
+
+        # Dynamic programming matrix        
+        dp = [[False for i in range(rows + 1)] for j in range(columns + 1)]
+    
+        # Match if both string and pattern are empty
+        dp[0][0] = True
+
+        

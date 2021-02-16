e324ba525b581ed3ff3cbcffb15a1c59f43d1bb2
milostiv <milostrifkovic89@gmail.com>
2021-02-16 19:43:12 +0100

    Regular Expression Matching

--- leetCode/python/p10_RegularExpressionMatching/lc10.py
+++ leetCode/python/p10_RegularExpressionMatching/lc10.py
@@ -20,9 +20,67 @@ class Solution:
             return False
 
         # Dynamic programming matrix        
-        dp = [[False for i in range(rows + 1)] for j in range(columns + 1)]
+        dp = [[False for j in range(columns + 1)] for i in range(rows + 1)]
     
         # Match if both string and pattern are empty
         dp[0][0] = True
 
-        # Nastavak sutra        
+        # For patterns with *
+        for i in range(2, columns + 1):
+            if p[i - 1] == '*':
+                dp[0][i] = dp[0][i-2]
+
+        # For remaining characters
+        for i in range(1, rows + 1):
+            for j in range(1, columns + 1):
+                if s[i - 1] == p[j - 1] or p[j - 1] == '.':
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif j > 1 and p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 2]
+                    if p[j - 2] == '.' or p[j - 2] == s[i - 1]:
+                        dp[i][j] = dp[i][j] or dp[i - 1][j]
+        
+        return dp[rows][columns]
+
+        
+    def test_function(self, s: str, p: str, sol: bool, test_num: int): 
+        start_time = time.time() 
+        tmp = self.isMatch(s, p)
+        end_time = time.time()
+        
+        if tmp == sol:
+            print('\nTEST' + str(test_num) + ': PASSED IN ' + '{:.2f}us'.format((end_time - start_time) * 1000000))            
+        else:
+            print('\nTEST FAILED') 
+
+sol = Solution()
+
+# Test 1
+
+s = 'aa'
+p = 'a'
+sol.test_function(s, p, False, 1)
+
+# Test 2
+
+s = 'aa'
+p = 'a*'
+sol.test_function(s, p, True, 2)
+
+# Test 3
+
+s = 'ab'
+p = '.*'
+sol.test_function(s, p, True, 3)
+
+# Test 4
+
+s = 'aab'
+p = 'c*a*b'
+sol.test_function(s, p, True, 4)
+
+# Test 5
+
+s = 'mississippi'
+p = 'mis*is*p*.'
+sol.test_function(s, p, False, 5)

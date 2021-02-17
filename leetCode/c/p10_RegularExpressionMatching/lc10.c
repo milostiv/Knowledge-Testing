0c0ba904190fec71d36078bd036fe278676b50fa
milostiv <milostrifkovic89@gmail.com>
2021-02-17 21:39:31 +0100

    Not yet finished

--- leetCode/c/p10_RegularExpressionMatching/lc10.c
+++ leetCode/c/p10_RegularExpressionMatching/lc10.c
@@ -7,10 +7,34 @@ The matching should cover the entire input string (not partial). */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <time.h>
 
 bool isMatch(char * s, char * p){
+    int rows = strlen(s);
+    int columns = strlen(p);     
+   
+    // Both strings empty
+    if(rows == 0 && columns == 0)
+        return true;     
 
+    // Patern empty
+    if(columns == 0) 
+        return false;
+
+    bool dp[rows + 1][columns + 1];
+    
+    // Initialize array with false values
+    memset(dp, false, sizeof dp);
+
+    for(int i=0; i<rows+1; i++) {
+        for(int j=0; j<columns+1; j++) {
+            printf("%d ", dp[i][j]);
+        }
+        printf("\n");
+    }
+     
+    //printf("%d, %d", rows, columns);
 }
 
 /* Test */
@@ -37,6 +61,11 @@ void testFunction(char * s, char * p, bool sol, int testNum){
 }
 
 int main(void){
+
+    char s[] = "aa";
+    char p[] = "a";
+
+    isMatch(s, p);
      
     return EXIT_SUCCESS;
 }

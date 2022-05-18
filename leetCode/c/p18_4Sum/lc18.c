c03ddab7224b5b8fcdfa4288b07bcc50a1e0c99e
milostiv <milostrifkovic89@gmail.com>
2022-05-18 18:06:50 +0200

    4Sum

--- leetCode/c/p18_4Sum/lc18.c
+++ leetCode/c/p18_4Sum/lc18.c
@@ -101,16 +101,16 @@ int main(void) {
 	int numsSize = sizeof(nums1)/sizeof(int);
 	int** answer = allocateMatrixInt(3, COLUMN_SIZE);
 	
-	//answer = fourSum(nums1, numsSize, target1, returnSize, returnColumnSizes);
-	//printMatrixInt(answer, *returnSize, COLUMN_SIZE);
+	answer = fourSum(nums1, numsSize, target1, returnSize, returnColumnSizes);
+	printMatrixInt(answer, *returnSize, COLUMN_SIZE);
 
 	printf("\n");
 
 	numsSize = sizeof(nums2)/sizeof(int);
 	//reallocateMatrixInt(answer, 1, COLUMN_SIZE);
 
-	answer = fourSum(nums2, numsSize, target2, returnSize, returnColumnSizes);
-	printMatrixInt(answer, *returnSize, COLUMN_SIZE);
+	//answer = fourSum(nums2, numsSize, target2, returnSize, returnColumnSizes);
+	//printMatrixInt(answer, *returnSize, COLUMN_SIZE);
 
 	freeAllocatedMatrixInt(answer, *returnSize);	
 

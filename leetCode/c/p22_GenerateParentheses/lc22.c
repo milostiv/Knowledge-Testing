1fc2142e1a03c5d9b07bf1134b87a13a2c4dfd27
milostiv <milostrifkovic89@gmail.com>
2022-06-04 17:48:27 +0200

    Generate Parentheses

--- leetCode/c/p22_GenerateParentheses/lc22.c
+++ leetCode/c/p22_GenerateParentheses/lc22.c
@@ -2,65 +2,69 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../../../mylib/mylib.h"
 
 #define MAX_SIZE 100
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-char** generateParenthesis(int n, int* returnSize) {
+static char str[MAX_SIZE];
 
-	if(n<=0) 
-		return NULL;
-
-	int *row, col;
-
-	*row = 10; // Randomly selected number, will be reallocated if exceeded
-	col = 2*n;
-	
-	char** matrix = allocateMatrixChar(row, col);	
-
-		
-	
-	return matrix;
+void _generateParenthesis(int pos, int n, int open, int close) {
+ 
+    if(close == n) {
+        printf("%s \n", str);
+        
+		return;
+    }
+    else {
+        if(open > close) {
+        	str[pos] = '}';
+        	_generateParenthesis(pos + 1, n, open, close + 1);
+        }
+ 
+        if(open < n) {
+            str[pos] = '{';
+            _generateParenthesis(pos + 1, n, open + 1, close);
+        }
+    }	
 }
 
-void _generateParenthesis(int pos, int n, int open, int close, char** matrix, int row) {
+void generateParenthesis(int n, int* returnSize) {
 
-	static char str[MAX_SIZE];	
-	static int currRow = 0;
-
-	if(close == n) {
-		matrix[currRow++][0] = str;
-		
-		/* Extend matrix if needed */
-
-		return;
-	}
-	else {
-		if(open > close) {
-			str[pos] = ')';
-			_generateParenthesis(pos + 1, n, open, close + 1, matrix, currRow);
-		}
-		
-		if(open < n) {
-			str[pos] = '(';
-			_generateParenthesis(pos + 1, n, open + 1, close, matrix, currRow);
-		}	
-	}
+	if(n > 0) 
+		_generateParenthesis(0, n, 0, 0);
+	
+	return;
 }
 
 int main(void) {
 
+	int n;
 	int* returnSize;
-	char** matrix;
 
-	matrix = generateParenthesis(2, returnSize);
+	// Test 1:
 
-	freeAllocatedMatrixChar(matrix, 10);
+	n = 3;	
+	printf("N = %d:\n", n);
+	generateParenthesis(n, returnSize);
+	memset(str,0,sizeof(str));	// Clear string
+	printf("\n");
 
-	printf("\nNot yet finished!\n\n");
+	// Test 2:
+
+	n = 1;	
+	printf("N = %d:\n", n);
+	generateParenthesis(n, returnSize);	
+	memset(str,0,sizeof(str));	// Clear string
+	printf("\n");
+
+	// Test 3:	
+
+	n = 4;	
+	printf("N = %d:\n", n);
+	generateParenthesis(n, returnSize);	
+	memset(str,0,sizeof(str));	// Clear string
+	printf("\n");
 
 	return EXIT_SUCCESS;
 }

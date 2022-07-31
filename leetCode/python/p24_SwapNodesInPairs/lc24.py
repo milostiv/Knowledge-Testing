1e36d2ef428d716dff2b5cede643212706f20c34
milostiv <milostrifkovic89@gmail.com>
2022-07-31 16:09:10 +0200

    Swap Nodes In Pairs

--- leetCode/python/p24_SwapNodesInPairs/lc24.py
+++ leetCode/python/p24_SwapNodesInPairs/lc24.py
@@ -9,9 +9,7 @@ class ListNode:
 class Solution:
 	'''
 		Given a linked list, swap every two adjacent nodes and return its head. 
-
 		You must solve the problem without modifying the values in the list's nodes 
-
 		(i.e., only nodes themselves may be changed.)
 	'''
 	def __init__(self):

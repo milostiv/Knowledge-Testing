75ecd39f3a2e86c2750cf60c875650052b9d6351
milostiv <milostrifkovic89@gmail.com>
2022-08-13 12:54:00 +0200

    Not yet finished!

--- leetCode/c/p28_ImplementStrStr/lc28.c
+++ leetCode/c/p28_ImplementStrStr/lc28.c
@@ -1,8 +1,36 @@
+/* Implement strStr(). Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack. */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../../../mylib/mylib.h"
+
+int strStr(char * haystack, char * needle) {
+
+	int count = 0;
+	char ch = haystack[0];
+	int haystackSize = strlen(haystack);
+	int needleSize = strlen(needle);
+
+	while(ch != '\0') {
+		
+		count++;
+		ch = haystack[count];
+	}
+
+	return count;
+}
 
 int main(void) {
 
-	printf("Not yet finished!\n");
+	/* Test 1: */
+
+	printf("Test 1: \n\n");
+
+	char *haystack1 = "hello";
+	char *needle1 = "ll";
+
+	int count = strStr(haystack1, needle1);
+
+	printf("Count %d\n", count);
 }

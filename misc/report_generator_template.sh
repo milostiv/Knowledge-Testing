e6cc796c2f68a1b26611aef7fefb0f44b4a00d89
milostiv <milostrifkovic89@gmail.com>
2022-12-24 13:47:17 +0100

    Added date and user info

--- misc/report_generator_template.sh
+++ misc/report_generator_template.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-title="Report"
+TITLE="Report for $HOSTNAME"
+CURRENT_TIME=$(date +"%x %r %Z")
+TIME_STAMP="Generated $CURRENT_TIME, by $USER"
 
 echo "<HTML>
 	<HEAD>
-			<TITLE>$title</TITLE>
+			<TITLE>$TITLE</TITLE>
 	</HEAD>
 	<BODY>
-			Report body
+			<H1>$TITLE</H1>
+			<P>$TIME_STAMP</P>
 	</BODY>
 </HTML>" > report.html

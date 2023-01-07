76b93ebb845583ddd0c05e33dfa8beb0d134fed3
milostiv <milostrifkovic89@gmail.com>
2023-01-07 12:36:19 +0100

    Word Frequency solution added

--- leetCode/shell/p192_WordFrequency/lc192.sh
+++ leetCode/shell/p192_WordFrequency/lc192.sh
@@ -3,10 +3,16 @@
 # Write a bash script to calculate the frequency
 # of each word in a text file words.txt.
 # 
-# For simplicity sake, you may assume:
-# 
+# For simplicity sake, you may assume: 
 # - words.txt contains only lowercase characters and space ' ' characters.
 # - Each word must consist of lowercase characters only.
 # - Words are separated by one or more whitespace characters.
 
-echo "Not yet finished!"
+file="words.txt"
+
+# First trim all the empty spaces and new lines (tr ' ' '\n')
+# Then sort the leftover text (sort)
+# Then count the uniq words in text (uniq -c)
+# Sort output in reverse order (sort -r)
+# Then remove the empty spaces in the begining of the output (sed 's/^\s*//')
+tr ' ' '\n' < $file | sort | uniq -c | sort -r | sed 's/^\s*//'

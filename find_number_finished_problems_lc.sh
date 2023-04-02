23bc6ad054124a466909680d5cc740555067c1bc
milostiv <milostrifkovic89@gmail.com>
2023-04-02 16:25:41 +0200

    Script now updates the number of finished problems

--- find_number_finished_problems_lc.sh
+++ find_number_finished_problems_lc.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 echo "Number of completed LeetCode problems in C:"
-find leetCode/c/ -mindepth 1 -type f -name "*.c" -printf x | wc -c
+c=$(find leetCode/c/ -mindepth 1 -type f -name "*.c" -printf x | wc -c)
+echo "$c"
+
+# Change 5 line of README.md file to update the number of finished C/C++ problems
+sed -i "5 s/.*/- [ ] [$c\/50] Complete 50 *LeetCode* problems in **C\/C++**/" README.md
 
 echo "Number of completed LeetCode problems in Python:"
-find leetCode/python/ -mindepth 1 -type f -name "*.py" -printf x | wc -c
+py=$(find leetCode/python/ -mindepth 1 -type f -name "*.py" -printf x | wc -c)
+echo "$py"
+
+# Change 5 line of README.md file to update the number of finished Python problems
+sed -i "6 s/.*/- [ ] [$py\/50] Complete 50 *LeetCode* problems in **Python**/" README.md
